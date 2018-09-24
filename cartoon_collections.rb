45f93670ce77402b1b71b56793cd4f2b4f650d52
Maayan Kfir <mayankfir@gmail.com>
2018-09-24 15:07:44 +0000

    automatically backed up by learn

---
+++ cartoon_collections.rb
@@ -0,0 +1,18 @@
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, i|
+    puts "#{i+1}. #{name}"
+end
+
+def summon_captain_planet(veggies)
+  veggies.collect {|x| "#{x}".capitalize + "!" }
+end
+
+def long_planeteer_calls(calls)
+  calls.any? {|word| word.length > 4}
+ end
+ 
+ def find_the_cheese(food)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  food.find {|cheese| cheese_types.include?(cheese)}
+end
